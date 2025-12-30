f9e8d7070bc41cef17172278e3597ee1c2940f15
Christos Maragkos <whitetowersoftware@gmail.com>
2025-12-30 02:39:56 +0200

    Delete redundant cli project

--- src/Sharpie.CLI/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Sharpie.Core;
-
-Console.WriteLine("Booting Sharpie...");
-var mobo = new Motherboard();
-mobo.Run();
-Console.WriteLine("Sharpie stopped.");
